d4e0cd91e9f2eb708a5e7744227ac370b7604696
João P. Meneguesso <jpm83633@gmail.com>
2022-04-01 08:26:02 -0300

    Nomes das cidades no sql

--- city.sql
+++ city.sql
@@ -2,3 +2,6 @@ create table city(
     idCity int primary key AUTO_INCREMENT,
     nameCity varchar(300) NOT NULL,
 );
+
+INSERT INTO `city` (`idCity`, `nameCity`) VALUES (NULL, 'Sumaré'), (NULL, 'Hortolândia ');
+
