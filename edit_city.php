baca4468e1e6fa3222a2d43c86d32e69c48f57d6
João P. Meneguesso <jpm83633@gmail.com>
2022-04-22 11:55:05 -0300

    Editar cidade

---
+++ edit_city.php
@@ -0,0 +1,54 @@
+<?php
+include '../connect.php';
+// include '../checklogin.php';
+$id = $_GET['id'];
+$nameCity = $_GET['cityName'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center><h1>Editar Cidade</h1></center>
+    <form method="POST">
+        <label> Selecione a cidade: </label><br>
+        <select name="city">
+        <?php
+        echo "<option value=$>$nameCity</option>";
+
+        $sqlCity= mysqli_query($con, "select * from city");
+                                    
+        while($item = mysqli_fetch_assoc($sqlCity))
+        {
+            $nomeItem = utf8_encode($item['nameCity']);
+            $idCity = $item['idCity'];
+            echo "                                
+                <option value=$idCity>$nomeItem</option>                                
+            ";
+        }
+        ?>
+        </select>
+        <br>
+        <br>
+        <input type="text" name="newName" placeholder="Novo nome da cidade">
+        <input type="submit" name="edit" value="Confirmar">
+    </form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['edit'])){
+    $newCityName = $_POST['newName'];
+    $i = "update city set nameCity = '$newCityName' where idCity='$id'";
+    mysqli_query($con, $i);
+    header('location: city.php');
+}
+
+?>
