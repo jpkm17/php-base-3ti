9ebd3f523a2b7a5fb27ecad5ffe9c3972e7ec3de
João P. Meneguesso <jpm83633@gmail.com>
2022-06-02 11:57:27 -0300

    alterações

--- home.php
+++ home.php
@@ -6,59 +6,6 @@
     $qu= mysqli_query($con, $s);
     $f=mysqli_fetch_assoc($qu);
 ?>
-<!-- <html>
-    <head>
-        
-    </head>
-    <body>
-<table>
-    <tr>
-        <td>
-            Name
-        </td><td>
-            
-<?php // echo $f['name'];?>
-            </td>
-    </tr>
-    <tr>
-    <td> Username</td>
-    
-    <td>
-            
-<?php
-//echo $f['username'];?>
-        </td></tr>
-<tr><td> Password</td>
-    <td>
-<?php
-//echo $f['password']."<br>";?>
-    </td></tr>
-  <tr><td> City </td>   <td>           
- <?php
- //echo $f['city']."<br>";?></td></tr>
-  <tr>
-      <td>Gender</td>
-      <td><?php
-//echo $f['gender']."<br>";?></td>
-  </tr>
-  <tr><td> Image</td><td>
-
- <img src="<?php
-//echo $f['image'];?>" width="100px" height="100px">
-</td>
-
-</tr>
-</table>
-<a href="edit.php">Edit</a>
-<a href="delete.php">Delete</a>
-<a href="logout.php">Logout</a>
-<a href="reg_city.php">City</a>
-<a href="dashboard.php">Dashboard</a>    
-</body>
-</html> -->
-
-<!-- insira o codigo a partir daqui -->
-
 
 <!DOCTYPE html>
 <html lang="en">
