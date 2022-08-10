6387c2777d30261c761af6f73704a1b234225692
João P. Meneguesso <75391591+jpkm17@users.noreply.github.com>
2022-08-10 08:27:47 -0300

    correção mysql

--- sql/city.sql
+++ sql/city.sql
@@ -1,6 +1,6 @@
 create table city(
     idCity int primary key AUTO_INCREMENT,
-    nameCity varchar(300) NOT NULL,
+    nameCity varchar(300) NOT NULL
 );
 
-INSERT INTO `city` (`idCity`, `nameCity`) VALUES (NULL, 'Sumaré'), (NULL, 'Hortolândia ');
+INSERT INTO `city` (`idCity`, `nameCity`) VALUES (NULL, 'Sumaré'), (NULL, 'Hortolândia');
