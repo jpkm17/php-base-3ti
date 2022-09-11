0e5020b93ea2ae4886c06d6b351b4a54f5dffa21
João P. Meneguesso <jpm83633@gmail.com>
2022-09-11 20:50:16 -0300

    Insert para as tabelas

--- sql/produto.sql
+++ sql/produto.sql
@@ -3,6 +3,10 @@ create table genero(
     nomeGenero varchar(100)
 );
 
+INSERT INTO `genero` (`idGenero`, `nomeGenero`) VALUES
+(1, 'Comedia'),
+(2, 'Ação');
+
 create table produto(
     idProduto int primary key auto_increment,
     nomeProduto varchar(100) not null,
@@ -11,3 +15,7 @@ create table produto(
     fk_idGenero int not null,
     FOREIGN KEY (fk_idGenero) REFERENCES genero(idGenero)
 );
+
+INSERT INTO `produto` (`idProduto`, `nomeProduto`, `preçoProduto`, `quantidade`, `fk_idGenero`) VALUES
+(null, 'Viagem ao centro da terra', 25, 100, 2),
+(null, 'Os trapalhoes', 15, 90 , 1);
