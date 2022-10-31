5752fad7daa9dd4f0693be70fc6b05ee78325c15
João P. Meneguesso <jpm83633@gmail.com>
2022-10-31 20:39:02 -0300

    Update editarProduto.php

--- editarProduto.php
+++ editarProduto.php
@@ -7,15 +7,28 @@ $nomeProduto = $_GET['nomeProduto'];
 $preço = $_GET['preço'];
 $nomeGenero = $_GET['genero'];
 $quantidade = $_GET['quantidade'];
-?> 
-  
+$imagem = $_GET['img'];
+
+
+// if($_FILES['f1']['name']){
+//   move_uploaded_file($_FILES['f1']['tmp_name'], "image/".$_FILES['f1']['name']);
+//   $img="image/".$_FILES['f1']['name'];
+//   }
+//   else{
+//       $img=$_POST['img1'];
+//   }
+
+// 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | User Profile</title>
-
+  <title>Editar produto</title>
+  <?php include './components/iconGanes.php'; ?>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -23,117 +36,107 @@ $quantidade = $_GET['quantidade'];
   <!-- Theme style -->
   <link rel="stylesheet" href="./AdminLTE-3.2.0/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
-  
-  <!-- Navbar -->
-  <?php include './components/nav.php'?>
-  <!-- /.navbar -->
+  <div class="wrapper">
 
-  <!-- Main Sidebar Container -->
+    <!-- Navbar -->
+    <?php include './components/nav.php' ?>
+    <!-- /.navbar -->
 
-  <?php include './components/mainSideBar.php' ?>
+    <!-- Main Sidebar Container -->
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Profile</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">User Profile</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+    <?php include './components/mainSideBar.php' ?>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-3">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-3">
 
-            <!-- Profile Image -->
-            <div class="card card-primary card-outline">
-              <div class="card-body box-profile">
-                <div class="text-center">
-                <!-- <img src="<?php //echo $f['image']?>" width="100px" height="100px"> -->
+              <!-- Profile Image -->
+              <div class="card card-primary card-outline">
+                <div class="card-body box-profile">
+                  <div class="text-center">
+                    <!-- <img src="<?php //echo $f['image']
+                                    ?>" width="100px" height="100px"> -->
+                  </div>
+
+                  <h3 class="profile-username text-center"><?php echo $f['name'] ?></h3>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+
+            </div>
+            <!-- /.col -->
+            <div class="col-md-9">
+              <div class="card">
+                <div class="card-header p-2">
+                  <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
+
+                  </ul>
+                  <!-- /.card-header -->
                 </div>
 
-                <h3 class="profile-username text-center"><?php echo $f['name']?></h3>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-
-          </div>
-          <!-- /.col -->
-          <div class="col-md-9">
-            <div class="card">
-              <div class="card-header p-2">
-                <ul class="nav nav-pills">
-                  <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
-
-                </ul>
-                <!-- /.card-header -->
-              </div>
-
                 <div class="card-body">
-                  <div class="tab-pane active" id="settings"> 
+                  <div class="tab-pane active" id="settings">
                     <form class="form-horizontal" method="POST">
                       <div class="form-group row">
                         <label for="inputName" class="col-sm-2 col-form-label">Produto</label>
                         <div class="col-sm-10">
-                        <input type="text" name="newName" class="form-control" value="<?php echo $nomeProduto?>">
+                          <input type="text" name="newName" class="form-control" value="<?php echo $nomeProduto ?>">
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputName" class="col-sm-2 col-form-label">Preço</label>
                         <div class="col-sm-10">
-                        <input type="number" name="newPrice" class="form-control" value="<?php echo $preço?>">
+                          <input type="text" name="newPrice" class="form-control" value="<?php echo $preço ?>">
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputEmail" class="col-sm-2 col-form-label">Quantidade
                         </label>
                         <div class="col-sm-10">
-                          <input type="number" name="newQuant" value="<?php echo $quantidade?>">
+                          <input type="number" name="newQuant" value="<?php echo $quantidade ?>">
                         </div>
                       </div>
                       <div class="form-group row">
-                        <label for="inputEmail" class="col-sm-2 col-form-label">Genero 
+                        <label for="inputEmail" class="col-sm-2 col-form-label">Genero
                         </label>
                         <div class="col-sm-10">
-                           <select name="Genero">
-                              <?php
-                                $sqlProduto = mysqli_query($con, "select * from genero");
+                          <select name="Genero">
+                            <?php
+                            $sqlProduto = mysqli_query($con, "select * from genero");
 
-                                while ($item = mysqli_fetch_assoc($sqlProduto)) {
-                                    $nomeItem = $item['nomeGenero'];
-                                    $idGenero = $item['idGenero'];
-                                    echo "<option value=$idGenero>$nomeItem</option>";
-                                }
-                              ?>
-                            </select>
+                            while ($item = mysqli_fetch_assoc($sqlProduto)) {
+                              $nomeItem = $item['nomeGenero'];
+                              $idGenero = $item['idGenero'];
+                              echo "<option value=$idGenero>$nomeItem</option>";
+                            }
+                            ?>
+                          </select>
+                        </div>
+                        <div class="col-sm-10">
+                          <img width="80px" height="120px" src="<?php echo $imagem; ?>">
+
+                          <br>
+                          <input type="file" name="f1">
                         </div>
                       </div>
-                      </div>
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <input type="submit" class="btn btn-danger" value="Concluir" name="edit">
-                          <!-- <button type="submit" class="btn btn-danger">Submit</button> -->
-                           
-                        </div>
-                      </div>
-                    </form>
                   </div>
+                  <div class="form-group row">
+                    <div class="offset-sm-2 col-sm-10">
+                      <input type="submit" class="btn btn-danger" value="Concluir" name="edit">
+                      <!-- <button type="submit" class="btn btn-danger">Submit</button> -->
+                    </div>
+                  </div>
+                  </form>
                 </div>
+              </div>
 
             </div>
             <!-- /.card -->
@@ -141,85 +144,93 @@ $quantidade = $_GET['quantidade'];
           <!-- /.col -->
         </div>
         <!-- /.row -->
-      </div><!-- /.container-fluid -->
+    </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
   </aside>
   <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- jQuery -->
-<!-- <script src="./AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script> -->
-<!-- Bootstrap 4 -->
-<!-- <script src="./AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
-<!-- AdminLTE App -->
-<!-- <script src="./AdminLTE-3.2.0/dist/js/adminlte.min.js"></script> -->
-<!-- AdminLTE for demo purposes -->
-<!-- <script src="./AdminLTE-3.2.0/addist/js/demo.js"></script> -->
+  <!-- jQuery -->
+  <script src="./AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="./AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="./AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <!-- <script src="./AdminLTE-3.2.0/addist/js/demo.js"></script> -->
 </body>
+
 </html>
 
 <?php
-  $usuario = "select * from reg where id='$_SESSION[id]'";  
-  $query = mysqli_query($con, $usuario);
-  $ct=mysqli_fetch_assoc($query);
-  
-      if (isset($_POST['edit'])) {
-          $novoProduto = $_POST['newName'];
-          $novoPreço = $_POST['newPrice'];
-          $novoGenero = $_POST['Genero'];
-          $novaQuantidade = $_POST['newQuant'];
-          $b= "update produto set nomeProduto = '$novoProduto', preçoProduto='$novoPreço', fk_idGenero='$novoGenero', quantidade='$novaQuantidade' where idProduto='$id'";
-          mysqli_query($con, $b);
-          // header('location: produto.php');
-          echo "<script>location.href='produto.php'</script>";
+$usuario = "select * from reg where id='$_SESSION[id]'";
+$query = mysqli_query($con, $usuario);
+$ct = mysqli_fetch_assoc($query);
 
-          function logMsg($id, $nameProduto,$novoProduto, $cota) {
-            $level = 'info'; 
-            $file = './log/main.log';
-            // ct --> coluna da tabela
-            $msg = ''. $cota['name']. ', Alterou informacao(s) do: ' .$nameProduto .' dos produtos. ID: ' .$id;
-            // variável que vai armazenar o nível do log (INFO, WARNING ou ERROR)
-            $levelStr = '';
-        
-            // verifica o nível do log
-            switch ($level) {
-                case 'info':
-                    // nível de informação
-                    $levelStr = 'INFO';
-                    break;
-        
-                case 'warning':
-                    // nível de aviso
-                    $levelStr = 'WARNING';
-                    break;
-        
-                case 'error':
-                    // nível de erro
-                    $levelStr = 'ERROR';
-                    break;
-            }
-        
-            date_default_timezone_set('America/Sao_Paulo');
-            $date = date('Y-m-d H:i:s');
-        
-            $msg = sprintf( "[%s] [%s]: %s%s", $date, $levelStr, $msg, PHP_EOL );
-        
-            file_put_contents($file, $msg, FILE_APPEND);
-        }
-          logMsg($id, $nomeProduto,$newProduct, $ct);
-        }
+if (isset($_POST['edit'])) {
+  $novoProduto = $_POST['newName'];
+  $novoPreço = $_POST['newPrice'];
+  $novoGenero = $_POST['Genero'];
+  $novaQuantidade = $_POST['newQuant'];
+  
+  if($_FILES['f1']['name']){
+    move_uploaded_file($_FILES['f1']['tmp_name'], "image/".$_FILES['f1']['name']);
+    $img="image/".$_FILES['f1']['name'];
+  }
+
+  $b = "update produto set nomeProduto = '$novoProduto', preçoProduto='$novoPreço', fk_idGenero='$novoGenero', quantidade='$novaQuantidade', img='$img' where idProduto='$id'";
+  mysqli_query($con, $b);
+  // echo "<script>location.href='produto.php'</script>";
+
+  
+  
+  
+  
+  
+  
+  function logMsg($id, $nameProduto, $novoProduto, $cota)
+  {
+    $level = 'info';
+    $file = './log/main.log';
+    // ct --> coluna da tabela
+    $msg = '' . $cota['name'] . ', Alterou informacao(s) do: ' . $nameProduto . ' dos produtos. ID: ' . $id;
+    // variável que vai armazenar o nível do log (INFO, WARNING ou ERROR)
+    $levelStr = '';
+
+    // verifica o nível do log
+    switch ($level) {
+      case 'info':
+        // nível de informação
+        $levelStr = 'INFO';
+        break;
+
+      case 'warning':
+        // nível de aviso
+        $levelStr = 'WARNING';
+        break;
+
+      case 'error':
+        // nível de erro
+        $levelStr = 'ERROR';
+        break;
+    }
+
+    date_default_timezone_set('America/Sao_Paulo');
+    $date = date('Y-m-d H:i:s');
+
+    $msg = sprintf("[%s] [%s]: %s%s", $date, $levelStr, $msg, PHP_EOL);
+
+    file_put_contents($file, $msg, FILE_APPEND);
+  }
+  logMsg($id, $nomeProduto, $newProduct, $ct);
+}
 ?>
